058845ae6b27eb0f9bdd8b8a88b91fc5b5c64a34
GasFurr <gasfurri@gmail.com>
2025-05-09 12:25:35 +0000

    Update settings.toml

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const allocator = gpa.allocator();
 
-pub fn main() void {
-    std.debug.print("Hello, B3T!\n", .{});
-}
+pub fn main() void {}
