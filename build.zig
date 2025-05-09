ba55fa873884b85e7402a30b21b44ea520fed59b
GasFurr <gasfurri@gmail.com>
2025-05-09 14:32:04 +0000

    Replace toml parser Instead of zig-toml we now use microwave (https://github.com/edqx/microwave) library.

--- build.zig
+++ build.zig
@@ -6,18 +6,18 @@ pub fn build(b: *std.Build) void {
 
     // create executable binary target
     const exe = b.addExecutable(.{
-        .name = "Game",
+        .name = "b3t-bin",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     // Add toml dependency.
-    const zig_toml_dep = b.dependency("zig_toml", .{}); // Match the .zon key name
+    const zig_toml_dep = b.dependency("microwave", .{}); // Match the .zon key name
 
     // Add its module using the correct name
-    exe.root_module.addImport("toml", // Your preferred import name
-        zig_toml_dep.module("zig-toml") // Actual module name from the dependency
+    exe.root_module.addImport("microwave", // Your preferred import name
+        zig_toml_dep.module("microwave") // Actual module name from the dependency
     );
 
     // Place binary in root folder
@@ -37,43 +37,55 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     // Release package configuration
-    //const exe_release = b.addExecutable(.{
-    //    .name = "b3t-bin",
-    //    .root_source_file = b.path("src/main.zig"),
-    //    .target = target,
-    //    .optimize = .ReleaseSafe,
-    //});
+    const exe_release = b.addExecutable(.{
+        .name = "b3t-bin",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = .ReleaseSafe,
+    });
 
     // Create release directory structure
-    // const mkdir_release = b.addSystemCommand(&.{"mkdir"});
-    // mkdir_release.addArgs(&.{ "-p", "release" });
+    const mkdir_release = b.addSystemCommand(&.{"mkdir"});
+    mkdir_release.addArgs(&.{ "-p", "release" });
 
     // Install binary to release directory
 
-    // const install_release = b.addInstallArtifact(exe_release, .{ .dest_dir = .{ .override = .{ .custom = "../release" } } });
+    const install_release = b.addInstallArtifact(exe_release, .{ .dest_dir = .{ .override = .{ .custom = "../release" } } });
 
     // Copy resources to release directory
-    // const copy_resources = b.addSystemCommand(&.{"cp"});
-    // copy_resources.addArgs(&.{ "-r", "resources/", "release/.todo" });
+    const copy_resources = b.addSystemCommand(&.{"cp"});
+    copy_resources.addArgs(&.{ "-r", "resources/", "release/" });
 
     // Create archive of release directory
-    // const create_archive = b.addSystemCommand(&.{
-    //    "tar",
-    //    "-czvf",
-    //    "b3t.tar.gz",
-    //    "release",
-    //});
+    const create_archive = b.addSystemCommand(&.{
+        "tar",
+        "-czvf",
+        "b3t.tar.gz",
+        "release",
+    });
 
     // Set up dependencies
-    //const release_step = b.step("release", "Create release package");
-    //release_step.dependOn(&mkdir_release.step);
-    //release_step.dependOn(&install_release.step);
-    //release_step.dependOn(&copy_resources.step);
-    //release_step.dependOn(&create_archive.step);
+    const release_step = b.step("release", "Create release package");
+    release_step.dependOn(&mkdir_release.step);
+    release_step.dependOn(&install_release.step);
+    release_step.dependOn(&copy_resources.step);
+    release_step.dependOn(&create_archive.step);
 
     // Ensure proper execution order
-    //install_release.step.dependOn(&mkdir_release.step);
-    //copy_resources.step.dependOn(&mkdir_release.step);
-    //create_archive.step.dependOn(&install_release.step);
-    //create_archive.step.dependOn(&copy_resources.step);
+    install_release.step.dependOn(&mkdir_release.step);
+    copy_resources.step.dependOn(&mkdir_release.step);
+    create_archive.step.dependOn(&install_release.step);
+    create_archive.step.dependOn(&copy_resources.step);
+
+    const clean_release = b.addSystemCommand(&.{
+        "rm",
+        "-rf",
+        "release/",
+        "b3t.tar.gz",
+        "b3t-bin",
+    });
+
+    const clean_step = b.step("clean", "Cleans up root from release files");
+
+    clean_step.dependOn(&clean_release.step);
 }
